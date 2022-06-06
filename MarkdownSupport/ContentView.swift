46c542a8204a71a56856a72db69131e8f250b040
zoop1259 <kdm432191@gmail.com>
2022-06-06 23:22:20 +0900

    markdown fin

--- MarkdownSupport/ContentView.swift
+++ MarkdownSupport/ContentView.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("""
+            # Welcome
+             **Hello World** - practice *markdown*
+            ios academy에서 배우는 markdown
+            [이 강의 링크](https://www.youtube.com/watch?v=aAX7kPCs8Vg)
+
+            swiftUI에서 한번 사용해보았다.
+            
+
+
+            ~~이것은 취소선이다~~
+            """)
+        //링크의 색을 바꿀수 있다.
+            .tint(Color.red)
             .padding()
     }
 }
