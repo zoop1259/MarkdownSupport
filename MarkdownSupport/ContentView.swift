c138feea3cc19a3b95f4d1390a9d933cdf62512d
zoop1259 <kdm432191@gmail.com>
2022-06-06 22:06:44 +0900

    Initial Commit

---
+++ MarkdownSupport/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarkdownSupport
+//
+//  Created by 강대민 on 2022/06/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarkdownSupport/MarkdownSupportApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownSupportApp.swift
+//  MarkdownSupport
+//
+//  Created by 강대민 on 2022/06/06.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownSupportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
